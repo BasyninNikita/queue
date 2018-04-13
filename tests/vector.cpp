9ec587bf0f29ecd1b0cfc2fdd6f6f383d35b7910
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2018-04-13 13:28:51 +0300

    Update vector.cpp

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -1,151 +1,39 @@
 #include <catch.hpp>
 #include <sstream>
 
-#include "vector.hpp"
+#include "queue.hpp"
 
 TEST_CASE("creating vector")
 {
-	vector_t<float> vector;
-	REQUIRE( vector.size() == 0 );
-	REQUIRE( vector.capacity() == 0 );
+	queue<int> queue;
+	REQUIRE( queue.head() == nullptr);
+	REQUIRE( queue.tail() == nullptr);
 }
 
-TEST_CASE("copying vector")
+TEST_CASE("pop and push")
 {
-	vector_t<int> vector;
-	vector.push_back(1);
-
-	vector_t<int> copy(vector);
-	REQUIRE( copy == vector );
+	queue<int> queue;
+	queue.push(1);
+	queue.push(2);
+	int sum=queue.pop;
+	sum+=queue.pop;
+	REQUIRE( sum == 3 );
 }
-
-TEST_CASE("assigning vector")
+TEST_CASE("throw")
 {
-	vector_t<int> vector1;
-	vector_t<int> vector2;
-
-	vector1.push_back(1);
-	vector2.push_back(2);
-
-	vector1 = vector2;
-	REQUIRE( vector1 == vector2 );
+	queue_t<int> q;
+	REQUIRE_THROWS_AS( q.pop() , Error);
 }
 
-TEST_CASE("equaling vector")
+TEST_CASE("copy")
 {
-	vector_t<short> vector1;
-	vector_t<short> vector2;
-
-	vector1.push_back(1);
-	vector2.push_back(1);
-
-	REQUIRE( vector1 == vector2 );
-
-	vector1.push_back(2);
-	REQUIRE( vector1 != vector2 );
+	queue_t<int> queue;
+	queue.push(1);
+	queue.push(2);
+	queue_t<int> queue1(queue);
+	int sum = queue1.pop();
+	sum = sum + queue1.pop();
+	REQUIRE( sum == 3 );
+	
 }
 
-TEST_CASE("indexing vector")
-{
-	vector_t<double> vector;
-
-	vector.push_back(1.1);
-
-	REQUIRE( vector[0] == 1.1 );
-
-	vector_t<double> const copy(vector);
-	REQUIRE( copy[0] == 1.1 );
-}
-
-TEST_CASE("pushing elements")
-{
-	vector_t<short> vector;
-
-	vector.push_back(1);
-	REQUIRE( vector.size() == 1 );
-	REQUIRE( vector.capacity() == 1 );
-
-	vector.push_back(2);
-	REQUIRE( vector.size() == 2 );
-	REQUIRE( vector.capacity() == 2 );
-
-	vector.push_back(3);
-	REQUIRE( vector.size() == 3 );
-	REQUIRE( vector.capacity() == 4 );
-
-	vector.push_back(4);
-	REQUIRE( vector.size() == 4 );
-	REQUIRE( vector.capacity() == 4 );
-
-	vector.push_back(5);
-	REQUIRE( vector.size() == 5 );
-	REQUIRE( vector.capacity() == 8 );
-}
-
-TEST_CASE("poping elements")
-{
-	vector_t<int> vector;
-
-	vector.push_back(1);
-	vector.push_back(2);
-	vector.push_back(3);
-	vector.push_back(4);
-	vector.push_back(5);
-	vector.push_back(6);
-
-	vector.pop_back();
-	REQUIRE( vector.size() == 5 );
-	REQUIRE( vector.capacity() == 8 );
-
-	vector.pop_back();
-	REQUIRE( vector.size() == 4 );
-	REQUIRE( vector.capacity() == 8 );
-
-	vector.pop_back();
-	REQUIRE( vector.size() == 3 );
-	REQUIRE( vector.capacity() == 8 );
-
-	vector.pop_back();
-	REQUIRE( vector.size() == 2 );
-	REQUIRE( vector.capacity() == 4 );
-
-	vector.pop_back();
-	REQUIRE( vector.size() == 1 );
-	REQUIRE( vector.capacity() == 2 );
-
-	vector.pop_back();
-	REQUIRE( vector.size() == 0 );
-	REQUIRE( vector.capacity() == 1 );
-}
-TEST_CASE("at_1")
-{
-	vector_t<int> vector;
-	vector.push_back(1);
-
-	REQUIRE( vector.at(0) == 1 );
-}
-
-TEST_CASE("at_2")
-{
-	vector_t<int> vector;
-	vector.push_back(3);
-	vector.push_back(7);
-
-	REQUIRE( vector.at(1) == 7 );
-}
-
-TEST_CASE("inc index1")
-{
-    vector_t<int> vector;
-	vector.push_back(1);
-    
-    REQUIRE_THROWS_AS( vector.at(1), std::out_of_range);
-}
-
-TEST_CASE("inc index2")
-{
-    vector_t<int> vector;
-	vector.push_back(1);
-    
-    REQUIRE_THROWS_AS( vector.at(10), std::out_of_range);
-}
